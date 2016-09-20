685822323bb576b05c119c09f1070c8dc35aa752
yufubin <yufubinfree@icloud.com>
2016-09-20 15:53:41 +0800

    addSome

--- fon.php
+++ fon.php
@@ -1,20 +1,12 @@
 <?php
-defined('E_DEPRECATED')
-    ? error_reporting(E_ALL & ~E_STRICT & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED)
-    : error_reporting(E_ALL & ~E_STRICT & ~E_WARNING & ~E_NOTICE);
-
-define('F_ROOT', __DIR__ . '/'); # 根目录
-define('F_CONFIG', F_ROOT . 'cfg/'); # 配置文件
-define('F_CLS', F_ROOT . 'cls/'); # 所有方法/类库
-
-require(F_ROOT . '/init.php'); # 初始化
+require('./init.php'); # 初始化
 \FON\J::getInstance()->setCallUrl('http://106.184.2.121:8080'); # alpha
 // \FON\J::getInstance()->setCallUrl('http://173.255.246.189:8080'); # beta
 // \FON\J::getInstance()->setAllUrl('lang', 'cn');
 \FON\J::getInstance()->setSaveAllResult();
 // \FON\J::getInstance()->setUseSaveAll();
 // \FON\J::getInstance()->setSaveResult('LocalDeal/cityList');
- 
+
 // \FON\J::getInstance()->s();
 // \FON\J::getInstance()->m('web/Deal/detail'); 
 // \FON\J::getInstance()->m('Vote/vote');
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require('./init.php'); # 初始化
+
+class Main {
+	public function main() {
+		die('as');
+	}
+}
+
+$main = new Main();
+$main->main();

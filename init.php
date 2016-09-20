b23d9b5fc3931dbaf57a6acb2b8756891743e087
yufubin <yufubinfree@icloud.com>
2016-09-20 16:19:20 +0800

    no message

--- init.php
+++ init.php
@@ -1,9 +1,18 @@
 <?php
-defined('E_DEPRECATED')
-    ? error_reporting(E_ALL & ~E_STRICT & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED)
-    : error_reporting(E_ALL & ~E_STRICT & ~E_WARNING & ~E_NOTICE);
+namespace FON;
 
-define('F_ROOT', __DIR__ . '/'); # 根目录
-define('F_CONFIG', F_ROOT . 'cfg/'); # 配置文件
-define('F_CLS', F_ROOT . 'cls/'); # 所有方法/类库
+$initPath = array(
+	'Config.php' => F_CONFIG,
+	'.cls.php'   => F_CLS,
+);
 
+# 引用配置文件
+foreach($initPath as $suffix => $path) {
+	foreach (scandir($path) as $fileName) {
+	    if(strpos($fileName, $suffix) === false) {
+	        continue;
+	    }
+
+	    require($path . $fileName);
+	}
+}
